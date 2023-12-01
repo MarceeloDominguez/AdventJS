bfd047c8838cb2694a50d6727d2b37f4d1b007db
marcelo dominguez <marceelodominguez@gmail.com>
2023-12-01 19:17:19 -0300

    01-cambiar-regalo-repetido

---
+++ 2023/01-cambiar-regalo-repetido/index.js
@@ -0,0 +1,28 @@
+// En la fábrica de juguetes del Polo Norte, cada juguete tiene un número de identificación único.
+// Sin embargo, debido a un error en la máquina de juguetes, algunos números se han asignado a más de un juguete.
+// ¡Encuentra el primer número de identificación que se ha repetido, donde la segunda ocurrencia tenga el índice más pequeño!
+// En otras palabras, si hay más de un número repetido, debes devolver el número cuya segunda ocurrencia aparezca primero en la lista.
+// Si no hay números repetidos, devuelve -1.
+
+function findFirstRepeated(gifts) {
+  //   const seen = {};
+  //   const firstRepeated = gifts.find((item) => {
+  //     if (seen[item]) return item;
+  //     seen[item] = true;
+  //   });
+  //   return firstRepeated !== undefined ? firstRepeated : -1;
+
+  const seen = new Set();
+
+  for (const item of gifts) {
+    if (seen.has(item)) return item;
+    seen.add(item);
+  }
+
+  return -1;
+}
+
+const giftIds = [2, 1, 3, 5, 3, 2];
+
+const result = findFirstRepeated(giftIds);
+console.log(result);
