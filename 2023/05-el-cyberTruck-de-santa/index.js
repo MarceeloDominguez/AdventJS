084c29f5222bc3d36750f21160693a8109a3fb1d
marcelo dominguez <marceelodominguez@gmail.com>
2023-12-05 18:32:06 -0300

    el cybertruck de santa

---
+++ 2023/05-el-cyberTruck-de-santa/index.js
@@ -0,0 +1,56 @@
+// Santa 🎅 está probando su nuevo trineo eléctrico, el CyberReindeer, en una carretera del Polo Norte.
+// La carretera se representa con una cadena de caracteres, donde:
+
+// . = Carretera
+// S = Trineo de Santa
+// * = Barrera abierta
+// | = Barrera cerrada
+// Ejemplo de carretera: S...|....|.....
+
+// Cada unidad de tiempo, el trineo avanza una posición a la derecha.
+// Si encuentra una barrera cerrada, se detiene hasta que la barrera se abra. Si está abierta, la atraviesa directamente.
+// Todas las barreras empiezan cerradas, pero después de 5 unidades de tiempo, se abren todas para siempre.
+// Crea una función que simule el movimiento del trineo durante un tiempo dado y devuelva un array de cadenas representando el estado de la carretera en cada unidad de tiempo:
+
+/* -> result:
+[
+  'S..|...|..', // estado inicial
+  '.S.|...|..', // avanza el trineo la carretera
+  '..S|...|..', // avanza el trineo la carretera
+  '..S|...|..', // el trineo para en la barrera
+  '..S|...|..', // el trineo para en la barrera
+  '...S...*..', // se abre la barrera, el trineo avanza
+  '...*S..*..', // avanza el trineo la carretera
+  '...*.S.*..', // avanza el trineo la carretera
+  '...*..S*..', // avanza el trineo la carretera
+  '...*...S..', // avanza por la barrera abierta
+]
+*/
+
+// El resultado es un array donde cada elemento muestra la carretera en cada unidad de tiempo.
+// Ten en cuenta que si el trineo está en la misma posición que una barrera, entonces toma su lugar en el array.
+
+function cyberReindeer(road, time) {
+  let arrRoad = [road];
+
+  const moveSleigh = (road, index) => {
+    let newRoad = Array.from(road);
+    newRoad[index] = "S";
+    newRoad[0] = ".";
+    return newRoad.join("");
+  };
+
+  for (let i = 0, index = 0; i < time - 1; i++) {
+    if (i === 4) road = road.replace(/\|/g, "*");
+    if (road[index + 1] !== "|") index++;
+
+    arrRoad.push(moveSleigh(road, index));
+  }
+
+  return arrRoad;
+}
+
+const road = "S..|...|..";
+const time = 10;
+const result = cyberReindeer(road, time);
+console.log(result);
